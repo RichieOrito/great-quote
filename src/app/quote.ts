d19ea7043b890f84845b8a2a79dee1120bea54d2
RichieOrito <richard.omondi@student.moringaschool.com>
2022-01-08 00:13:07 +0300

    This commit changes how we create the Goal blueprint class by using a constructor function and added date

--- src/app/quote.ts
+++ src/app/quote.ts
@@ -1,5 +1,8 @@
 export class Quote {
-  id!: number;
-  author!: string;
-  text!: string;
+  // id!: number;
+  // author!: string;
+  // text!: string;
+  // completeDate!: Date;
+  constructor(public id: number,public text: string,public author: string,public completeDate: Date){
+  }
 }
