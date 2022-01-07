a5229d633006ca612bc7a7496365ec3f7640dbf1
RichieOrito <richard.omondi@student.moringaschool.com>
2022-01-07 03:49:56 +0300

    This commit creates the quote component to handle the logic on quotes

---
+++ src/app/quote/quote.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+import { Quote } from '../quote';
+
+
+@Component({
+  selector: 'app-quote',
+  templateUrl: './quote.component.html',
+  styleUrls: ['./quote.component.css']
+})
+export class QuoteComponent implements OnInit {
+
+  quotes:Quote[] = [
+    {id:1,text:'Age is no barrier. It is a limitation you put on your mind' , author: 'Jackie Joyner-Kersee'},
+    {id:3,text:'To give anything less than your best, is to sacrifice the gift.',author:'Steve Prefontaine'},
+    {id:4,text:'People can say what they want about me. But I have got a big heart and will keep going.',author:'Tyson Furry'},
+    {id:5,text:'Only the disciplined ones in life are free. If you are undisciplined, you are a slave to your moods and your passions.',author:'Eliud Kipchoge'},
+    {id:6,text:'Success is no accident. It is hard work, perseverance, learning, studying, sacrifice and most of all, love of what you are doing or learning to do.',author:'Pele'},
+    {id:7,text:'The road to our dreams has many detours.',author:'Kurt Warner'}
+    ];
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
