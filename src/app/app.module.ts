d38915310633439f9fc2799a01c6af17d6d1d74f
RichieOrito <richard.omondi@student.moringaschool.com>
2022-01-07 20:47:52 +0300

    This commit adds the quote-form component to the root module declarations array

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { QuoteComponent } from './quote/quote.component';
 import { StrikethroughDirective } from './strikethrough.directive';
+import { QuoteFormComponent } from './quote-form/quote-form.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     QuoteComponent,
-    StrikethroughDirective
+    StrikethroughDirective,
+    QuoteFormComponent
   ],
   imports: [
     BrowserModule,
