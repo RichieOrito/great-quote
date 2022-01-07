6f0767c4bdf27d66c49340478e03e713e31b0682
RichieOrito <richard.omondi@student.moringaschool.com>
2022-01-07 03:35:54 +0300

    This commit created several quotes using the quote blueprint class

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Quote } from './quote';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'great-quotes';
+  quotes:Quote[] = [
+  {id:1,text:'Age is no barrier. It is a limitation you put on your mind' , author: 'Jackie Joyner-Kersee'},
+  {id:3,text:'To give anything less than your best, is to sacrifice the gift.',author:'Steve Prefontaine'},
+  {id:4,text:'People can say what they want about me. But I have got a big heart and will keep going.',author:'Tyson Furry'},
+  {id:5,text:'Only the disciplined ones in life are free. If you are undisciplined, you are a slave to your moods and your passions.',author:'Eliud Kipchoge'},
+  {id:6,text:'Success is no accident. It is hard work, perseverance, learning, studying, sacrifice and most of all, love of what you are doing or learning to do.',author:'Pele'},
+  {id:7,text:'The road to our dreams has many detours.',author:'Kurt Warner'}
+  ];
 }
